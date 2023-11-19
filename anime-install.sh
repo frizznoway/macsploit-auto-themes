d921e0d6df725de8e997fe95780a42bef9b92389
frizznoway <127583921+frizznoway@users.noreply.github.com>
2023-11-18 22:49:40 -0700

    Update anime-install.sh

--- anime-install.sh
+++ anime-install.sh
@@ -1,8 +1,10 @@
 main() {
     cd /Applications/MacSploit.app/Contents/Resources/CodeViewer_CodeViewer.bundle/Contents/Resources/ace.bundle
-    echo -e "downloading ur horrible theme.."
+    echo -e "anime theme installing.."
+    echo -e "anime theme 90% done.."
+    echo -e "receiving xferd.."
     curl "https://raw.githubusercontent.com/frizznoway/macsploit-auto-themes/main/anime.js" -o "./theme-tomorrow_night.js"
-    echo -e "automatic theme install by frizznoway"
+    echo -e "automatic theme install by forensnoway"
 }
 
 main
