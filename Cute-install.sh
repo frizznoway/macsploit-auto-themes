3aebf6837cdbfe53e3a8ac3b3d1ddf26b88558df
Forens <127583921+frizznoway@users.noreply.github.com>
2023-11-21 18:57:38 -0700

    Create Cute-install.sh

---
+++ Cute-install.sh
@@ -0,0 +1,10 @@
+main() {
+    cd /Applications/MacSploit.app/Contents/Resources/CodeViewer_CodeViewer.bundle/Contents/Resources/ace.bundle
+    echo -e "anime theme installing.."
+    echo -e "anime theme 90% done.."
+    echo -e "receiving xferd.."
+    curl "https://raw.githubusercontent.com/frizznoway/macsploit-auto-themes/main/cute" -o "./theme-tomorrow_night.js"
+    echo -e "automatic theme install by forensnoway"
+}
+
+main
